324ceeef92d259541555730532417e7966c88f65
EgCornel <egor.liney@gmail.com>
2024-01-20 15:50:01 +0300

    update EN

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 export default () => {
   console.log('Welcome to the Brain Games!');
 
-  const name = readlineSync.question('Могу я узнать ваше имя?');
-  console.log(`Привет, ${name}!\n`);
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${name}!\n`);
 };
