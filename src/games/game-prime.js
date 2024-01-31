a6139d3c5f8bc79c71bd3f6b78860e562b120351
EgCornel <egor.liney@gmail.com>
2024-01-31 15:42:15 +0300

    changing logic and name function

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -2,17 +2,17 @@ import logicGame from '../index.js';
 import getRandomNumber from '../random.js';
 
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const getPrimeNamber = (num) => {
-  for (let i = 2; i < Math.sqrt(num); i += 1) {
+const isPrimeNamber = (num) => {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
     }
   }
-  return num !== 1;
+  return num > 1;
 };
 const startGame = () => {
   const question = getRandomNumber(1, 100);
-  const answer = getPrimeNamber(question) ? 'yes' : 'no';
+  const answer = isPrimeNamber(question) ? 'yes' : 'no';
   return [question, answer];
 };
 export default () => {
